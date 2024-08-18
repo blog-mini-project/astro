27869932fbbd1c8a52ca91f3049d3a9929075d7f
aquaticcalf <aquaticcalf@proton.me>
2024-08-18 12:21:20 +0530

    post and comment end points work now

--- src/pages/api/post/create.ts
+++ src/pages/api/post/create.ts
@@ -1,22 +1,16 @@
 export const prerender = false
 
-// import type { APIRoute } from 'astro'
-// import { makePost } from '../../../lib/posts'
-// import { verifyToken } from '../../../lib/auth'
+import type { APIRoute } from "astro"
+import { makePost } from '../../../lib/posts'
 
-// export const post: APIRoute = async ({ request }) => {
-//     const { title, content, tags, author } = await request.json()
-//     const token = request.headers.get('Authorization')?.split(' ')[1]
+export const POST: APIRoute = async ({ locals, request }) => {
+    const user = locals.user.username
+    const { title, content, tags } = await request.json()
 
-//     if (!token) {
-//         return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 })
-//     }
-
-//     try {
-//         verifyToken(token)
-//         const newPost = await makePost(title, content, author, tags)
-//         return new Response(JSON.stringify(newPost), { status: 200 })
-//     } catch (error) {
-//         return new Response(JSON.stringify({ error: error.message }), { status: 400 })
-//     }
-// }
+    try {
+        const newPost = await makePost(title, content, user, tags)
+        return new Response(JSON.stringify(newPost), { status: 200 })
+    } catch (error) {
+        return new Response(JSON.stringify({ error: error.message }), { status: 400 })
+    }
+}
